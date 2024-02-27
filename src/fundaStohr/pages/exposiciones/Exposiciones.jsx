c9cfef6bbaeb3df0e74f7f3d36bc04a3c5e29927
Luis Gil <giluis2000@gmail.com>
2024-02-27 16:06:10 -0400

    exposiciones

--- src/fundaStohr/pages/exposiciones/Exposiciones.jsx
+++ src/fundaStohr/pages/exposiciones/Exposiciones.jsx
@@ -16,41 +16,52 @@ export const Exposiciones = () => {
         className="animate__animated animate__fadeIn"
       >
         <Container style={pageStyles.container}>
-          <h1 style={pageStyles.title}>Exposiciones</h1>
-          <hr
-            className="text-black d-none d-sm-block"
-            style={pageStyles.divider}
-          />
+          <h2 className="title d-flex justify-content-center">Exposiciones</h2>
           <Row>
-            <Col
-              style={pageStyles.col}
-              className="col-xl-6 col-lg-12 d-flex justify-content-center"
-            >
-              <img
-                src="assets/DibujandoEnArmonia1.jpg"
-                alt="La Fundación"
-                style={{ borderRadius: "34% 66% 14% 86% / 100% 7% 93% 0% " }}
-              />
+            <Col style={pageStyles.col} className="col-xl-6 col-lg-12">
+              <Row>
+                <p
+                  style={{
+                    color: "#555555",
+                    fontFamily: "sans-serif",
+                    textAlign: "justify",
+                  }}
+                >
+                  En mayo 2017 el Periódico El Sol de Margarita le realizó una
+                  entrevista a Carlos Stohr, quien refirió que la
+                  internacionalización de los artistas surgidos en el semillero
+                  insular, parecía estar muy lejos, con todo y que hay
+                  excelentes ponentes de las artes plásticas, unos nativos y
+                  otros que no, pero que se han formado en la Isla. "La
+                  internacionalización de la cultura nunca ha sido fácil, ni es
+                  un boleto que está “ahí”.
+                  <br />
+                  Percibía que habían muy buenos artistas, pero no tienen la
+                  suficiente promoción para expandirse a escala nacional y menos
+                  en el extranjero, por lo que cada artista debe ingeniársela
+                  para lograr promocionarse por sus propios medios, comenzando
+                  desde el terruño insular. Mostró gran preocupación de que el
+                  apoyo recibido no es suficiente para reflejar sus talentos y
+                  obras en otras zonas, dentro y fuera del país.
+                </p>
+              </Row>
+              <Row>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/fundastohr-image-gallery.appspot.com/o/Fotos%20Variadas%2FActividades1.jpeg?alt=media&token=6f062129-8027-4c9c-97fb-36b81a2d45f7"
+                  alt=""
+                />
+              </Row>
             </Col>
             <Col style={pageStyles.col} className="col-xl-6 col-lg-12">
-              <p style={pageStyles.text}>
-                En mayo 2017 el Periódico El Sol de Margarita le realizó una
-                entrevista a Carlos Stohr, quien refirió que la
-                internacionalización de los artistas surgidos en el semillero
-                insular, parecía estar muy lejos, con todo y que hay excelentes
-                ponentes de las artes plásticas, unos nativos y otros que no,
-                pero que se han formado en la Isla. "La internacionalización de
-                la cultura nunca ha sido fácil, ni es un boleto que está “ahí”.
-                <br />
-                Percibía que habían muy buenos artistas, pero no tienen la
-                suficiente promoción para expandirse a escala nacional y menos
-                en el extranjero, por lo que cada artista debe ingeniársela para
-                lograr promocionarse por sus propios medios, comenzando desde el
-                terruño insular. Mostró gran preocupación de que el apoyo
-                recibido no es suficiente para reflejar sus talentos y obras en
-                otras zonas, dentro y fuera del país.
-                <br />
-                <br />
+              <Row>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/fundastohr-image-gallery.appspot.com/o/Fotos%20Variadas%2FActividades2.jpeg?alt=media&token=19c7569a-c8d8-4cc4-b259-1d802a63d204"
+                  alt=""
+                  className="pb-5"
+                />
+              </Row>
+              <Row className="pb-5">
+                <hr className="py-2" />
                 “En Margarita hay artistas, pintores y escultores, pero hace
                 falta que se den a conocer porque muchos de ellos se quedan en
                 sus talleres, y ni en su casa los conocen”, Para Carlos Stohr la
@@ -69,7 +80,7 @@ export const Exposiciones = () => {
                 <br />
                 Los invitamos a visualizar las exposiciones virtuales y a seguir
                 de cerca nuestras muestras en museos y centros culturales.
-              </p>
+              </Row>
             </Col>
           </Row>
         </Container>

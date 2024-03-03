8007afeba869b88206e42039d44a0ab33c3a9384
Luis Gil <giluis2000@gmail.com>
2024-03-03 17:55:43 -0400

    google maps puesto

--- src/fundaStohr/pages/contacto/Contacto.jsx
+++ src/fundaStohr/pages/contacto/Contacto.jsx
@@ -1,39 +1,65 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import { pageStyles } from "../styles";
 import Footer from "../../components/Footer";
 import { DoubleTitle } from "../../components/DoubleTitle/DoubleTitle";
 import { CardsContacto } from "../../components/CardsContacto/CardsContacto";
-import "./contacto.css"
+import "./contacto.css";
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+
+const libraries = ["places"];
+const center = {
+  lat: 10.493995324866681,
+  lng: -66.85504776742661,
+};
 
 export const Contacto = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBlI1yE_OEe3FUNK4JVuVn64Il8Jkula8A",
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
   return (
     <Row>
-      <div id = "Contacto" style={{marginBottom: '0rem'}}>
-        <Container >
-          <DoubleTitle 
-            title={"contact"}
-            subtitle={"contáctanos"}
-          />
-           
-          <Row id = "contact" className="contact-info">
-          
-            <CardsContacto 
+      <div id="Contacto" style={{ marginBottom: "0rem" }}>
+        <Container>
+          <DoubleTitle title={"contact"} subtitle={"contáctanos"} />
+
+          <Row id="contact" className="contact-info">
+            <CardsContacto
               icono={"bi-geo-alt"}
               title={"Dirección"}
-              description={"Avenida Francisco de Miranda, Caracas 1060, Miranda, Venezuela"}
-            /> 
-            <CardsContacto 
+              description={
+                "Avenida Francisco de Miranda, Caracas 1060, Miranda, Venezuela"
+              }
+            />
+            <CardsContacto
               icono={"bi bi-phone"}
               title={"Teléfono"}
               description={" Telf: +58 414-1203262 "}
-            /> 
-            <CardsContacto 
+            />
+            <CardsContacto
               icono={"bi bi-envelope"}
               title={"Email"}
               description={"fundacion.carlos.stohr@gmail.com"}
             />
           </Row>
+          <div>
+            <GoogleMap
+              mapContainerStyle={{ width: "100%", height: "10rem" }}
+              zoom={14}
+              center={center}
+            >
+              <MarkerF position={center} />
+            </GoogleMap>
+          </div>
         </Container>
       </div>
       <Footer />
